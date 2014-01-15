399e719c80f52275e88cb0e912c57eff15272343
Musik <Musik@ibm-t41p>
2014-01-15 16:52:24 +0100

    -

--- power_capturelyseBeans/ejbModule/client/SessionBeanClient.java
+++ power_capturelyseBeans/ejbModule/client/SessionBeanClient.java
@@ -1,28 +1,58 @@
 package client;
 
 import interfaces.*;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 
 import javax.ejb.EJB;
 import javax.naming.Context;
 import javax.naming.NamingException;
 
+
+
 import serviceLocator.ServiceLocator;
 import sessionBeans.*;
 import client.ClientUtility;
+import interfaces.*;
 
 public class SessionBeanClient {
     @EJB
     private static SessionRemoteInterface remote;
+    
+   @EJB
+   private static VerbrauchVerwaltungInterface remote2;
 
-    public static void main(String[] args) throws IOException, NamingException {
+    public static void main(String[] args) throws IOException, NamingException ,ParseException {
 	
-	
 
+    	ServiceLocator locator = new ServiceLocator();
+    	String beanName = VerbrauchVerwaltungBean.class.getSimpleName();
 
+    	remote2 = (VerbrauchVerwaltungInterface) locator.getStateless(
+    		"pcl-eap", "power_capturelyseBeans", beanName,
+    		VerbrauchVerwaltungBean.class);
+    	
+    	
+    	String date1 = "2014-01-01 00:00:00";
+    	String date2 = "2014-01-10 00:00:00";
+    	SimpleDateFormat format1 = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+    	Date datumVon = (Date) format1.parse(date1);
+    	Date datumBis = (Date) format1.parse(date2);
+    	// Timestamp datumVon = new Timestamp(datum1.getTime());
+    	// Timestamp datumBis = new Timestamp(datum2.getTime());
+    	System.out.println(datumVon);
+    	System.out.println(datumBis);
+    	
+    
+    	remote2.getVerbraeucheAuswahl(1, datumVon, datumBis);
+    	
+    	System.out.print("Fertig");
+/*
 	BufferedReader console = new BufferedReader(new InputStreamReader(
 		System.in));
 
@@ -45,6 +75,8 @@ public class SessionBeanClient {
 	} else {
 	    System.out.println("Sie haben sich erfolgreich angemeldet, ");// +remote.whoIam());
 	}
+	
+	*/	
 
     }
 
@@ -86,10 +118,16 @@ public class SessionBeanClient {
 	// Fully qualified remote interface name
 	final String interfaceName = SessionRemoteInterface.class.getName();
 
-	// Create a look up string name
+     // Create a look up string name Stateless
+	String name = "ejb:" + appName + "/" + moduleName + "/" + distinctName
+		+ "/" + beanName + "!" + interfaceName;
+	System.out.println(name);
+	
+	/*// Create a look up string name Stateful
 	String name = "ejb:" + appName + "/" + moduleName + "/" + distinctName
 		+ "/" + beanName + "!" + interfaceName + "?stateful";
 	System.out.println(name);
+	*/
 	return name;
     }
 }
